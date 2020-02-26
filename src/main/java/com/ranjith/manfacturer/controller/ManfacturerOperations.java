9b4df4b1a67d3aaa58eadf6b67ed2b7b8c1ef8cf
Ranjith <mail.ranjith99@gmail.com>
2020-02-26 17:19:08 +0530

    Added Msgs for Success and Failure Cases Added Method to get Manufacturer Det

--- src/main/java/com/ranjith/manfacturer/controller/ManfacturerOperations.java
+++ src/main/java/com/ranjith/manfacturer/controller/ManfacturerOperations.java
@@ -1,5 +1,6 @@
 package com.ranjith.manfacturer.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ranjith.manfacturer.request.CommonRequest;
 import com.ranjith.manfacturer.response.CommonResponse;
+import com.ranjith.manfacturer.service.ManfacturerOperationsService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -14,22 +16,55 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @Slf4j
 public class ManfacturerOperations {
-	
-	@PostMapping(value="/get-Details" ,produces="application/json")
+
+	@Autowired
+	private ManfacturerOperationsService manufactOperServ;
+
+	@PostMapping(value = "/get-Details", consumes = "application/json", produces = "application/json")
 	public CommonResponse getManfacturerDetails(@RequestBody CommonRequest request) {
-		
+
 		CommonResponse response = new CommonResponse();
-		
+
 		log.info("**************** Fetching Manfacturer Details **********************");
-		log.info("Pass is "+request.getPassword());
-		
-		log.info(" Username is : {} and Pass is : {}",request.getUserName(),request.getPassword());
+		log.info("Pass is " + request.getPassword());
+
+		log.info(" Username is : {} and Pass is : {}", request.getUserName(), request.getPassword());
 
 		response.setMessage("Manfacturer Det Retrieved Succesfully");
 		response.setStatus("OK");
 
 		return response;
 
+	}
+
+	@PostMapping(value = "/get-By-Id", consumes = "application/json", produces = "application/json")
+	public CommonResponse getDetailsByManufacturerId(@RequestBody CommonRequest request) {
+
+		return manufactOperServ.getManufacturerDetById(request);
+	}
+
+	@PostMapping(value = "/get-By-Name", consumes = "application/json", produces = "application/json")
+	public CommonResponse getDetailsByManufacturerName(@RequestBody CommonRequest request) {
+		CommonResponse response = new CommonResponse();
+		return response;
+	}
+
+	@PostMapping(value = "/update-By-Id", consumes = "application/json", produces = "application/json")
+	public CommonResponse updateManfacturerById(@RequestBody CommonRequest request) {
+		CommonResponse response = new CommonResponse();
+		return response;
+	}
+
+	@PostMapping(value = "/add", consumes = "application/json", produces = "application/json")
+	public CommonResponse addManufacturer(@RequestBody CommonRequest request) {
+		CommonResponse response = new CommonResponse();
+		return response;
+	}
+
+	@PostMapping(value = "/delete", consumes = "application/json", produces = "application/json")
+	public CommonResponse deleteManufacturer(@RequestBody CommonRequest request) {
+		CommonResponse response = new CommonResponse();
+		return response;
 	}
 
 }
--- src/main/java/com/ranjith/manfacturer/repository/ManufacturerDetRepo.java
+++ src/main/java/com/ranjith/manfacturer/repository/ManufacturerDetRepo.java
@@ -1,9 +1,15 @@
 package com.ranjith.manfacturer.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ranjith.manfacturer.entity.ManufacturerDetails;
 
 public interface ManufacturerDetRepo extends JpaRepository<ManufacturerDetails,String>{
+	
+	public List<ManufacturerDetails> findByManufacturerId(String manufactId);
+	
+	public List<ManufacturerDetails> findByManufacturerName(String manufactName);
 
 }
