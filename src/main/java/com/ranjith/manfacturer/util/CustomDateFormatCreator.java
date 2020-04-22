b002559cce288630ac9311589ff128c1904ddc11
Ranjith <mail.ranjith99@gmail.com>
2020-04-22 13:28:52 +0530

    Remove Unused Code

--- src/main/java/com/ranjith/manfacturer/util/CustomDateFormatCreator.java
+++ src/main/java/com/ranjith/manfacturer/util/CustomDateFormatCreator.java
@@ -9,20 +9,13 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 
-public class CustomDateFormatCreator extends JsonSerializer<LocalDateTime> {    
-    @Override
-    public void serialize(LocalDateTime dateValue, JsonGenerator gen, SerializerProvider arg2) throws 
-        IOException, JsonProcessingException {      
+public class CustomDateFormatCreator extends JsonSerializer<LocalDateTime> {
+	@Override
+	public void serialize(LocalDateTime dateValue, JsonGenerator gen, SerializerProvider arg2)
+			throws IOException, JsonProcessingException {
 
-		/*
-		 * SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss a");
-		 * String formattedDate = formatter.format(dateValue);
-		 */
+		String convertedDate = dateValue.format(DateTimeFormatter.ofPattern("dd-MMM-yyyy HH:mm:ss a"));
+		gen.writeString(convertedDate);
 
-        String convertedDate = dateValue.format(DateTimeFormatter.ofPattern("dd-MMM-yyyy HH:mm:ss a"));
-        gen.writeString(convertedDate);
-
-    }
+	}
 }
-
-
